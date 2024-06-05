bf1629e50eaacf864db80fdca2d44e2075b3abba
Jan Motak <jan.motak@hotmail.cz>
2024-06-06 00:27:34 +0200

    feat(components, theme): install theme and ui library

--- src/pedromenicka.client/app/_utils/fetch.ts
+++ src/pedromenicka.client/app/_utils/fetch.ts
@@ -1,8 +1,9 @@
+// Generic fetch function
 export async function typedFetch<T>(url: string, options?: RequestInit): Promise<T[]> {
   const res = await fetch(`${process.env.API_BASE_URL}/${url}`, options);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error(`Failed to fetch data for route: /${url}`);
   }
 
   return (await res.json()) as Promise<T[]>;
--- src/pedromenicka.client/app/page.tsx
+++ src/pedromenicka.client/app/page.tsx
@@ -1,9 +1,16 @@
-import { typedFetch } from '@/app/utils/fetch';
+import { typedFetch } from '@/app/_utils/fetch';
+import { ModeToggle } from '@/app/temp-components/ThemeToggle';
+import { Button } from '@/components/ui/button';
 import { WeatherForecast } from '@/types/api';
 
 export default async function Home() {
   const weatherData = await typedFetch<WeatherForecast>('weatherforecast');
   console.log(weatherData);
 
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Jope</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>Hello its me</Button>
+      <ModeToggle />
+    </main>
+  );
 }
